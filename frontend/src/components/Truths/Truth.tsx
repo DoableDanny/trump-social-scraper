8eb2366813ca163538f032cbd83269a79c341ee4
doabledanny <dantheado@gmail.com>
2025-04-23 14:02:23 +0100

    Typed media_attachments

--- frontend/src/components/Truths/Truth.tsx
+++ frontend/src/components/Truths/Truth.tsx
@@ -3,13 +3,49 @@ import { useState } from "react";
 import { formatTime } from "./utils";
 import styles from "./Truths.module.css";
 
+export type MediaAttachment = {
+  id: string;
+  url: string;
+  meta: {
+    small?: {
+      size: string;
+      width: number;
+      height: number;
+      aspect: number;
+    };
+    original?: {
+      size?: string;
+      width: number;
+      height: number;
+      bitrate?: number;
+      duration?: number;
+      frame_rate?: string;
+      aspect?: number;
+    };
+    colors?: {
+      accent: string;
+      background: string;
+      foreground: string;
+    };
+  };
+  type: "image" | "video";
+  blurhash: string | null;
+  text_url: string | null;
+  processing: string;
+  remote_url: string | null;
+  description: string | null;
+  preview_url: string | null;
+  external_video_id: string | null;
+  preview_remote_url: string | null;
+};
+
 export type Truth = {
   id: number;
   content: string;
   external_id: number;
   timestamp: string;
   url: string;
-  media_attachments: any[];
+  media_attachments: MediaAttachment[];
   ai_summary: string;
 };
 
